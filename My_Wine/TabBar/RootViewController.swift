69f22b2e41a1e36f3b19af0ecc1ec8e627b07a0a
Karen <karen606>
2024-11-12 20:48:27 +0400

    fixed root folder

---
+++ My_Wine/TabBar/RootViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  My_Wine
+//
+//  Created by Karen Khachatryan on 05.10.24.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        if let tabBarVC = storyboard?.instantiateViewController(withIdentifier: "TabBarController") {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let window = windowScene.windows.first {
+                window.rootViewController = tabBarVC
+                window.makeKeyAndVisible()
+            }
+        }
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-My_Wine/Pods-My_Wine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_My_Wine : NSObject
+@end
+@implementation PodsDummy_Pods_My_Wine
+@end
